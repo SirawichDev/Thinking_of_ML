91fd5d8bda50aa3661b80ae92522f1e71a1b59d1
SirawichDev <sirawit0676@gmail.com>
2018-05-31 18:45:42 +0700

    using Logistic norm L2

--- Classification/Sklearn/sk_regression.py
+++ Classification/Sklearn/sk_regression.py
@@ -1,1 +1,17 @@
 from sklearn.linear_model import  LogisticRegression
+import numpy as np
+
+dataset = [[-3.215, 0],
+                [-1.234,0],
+                [0.974,0],
+                [3.231,1],
+                [1.2345,1],
+                [4.5122,1]]
+
+x = np.array(dataset)[:, 0:1]
+y = np.array(dataset)[:,1]
+print(x)
+LR = LogisticRegression(C=1.0 , penalty='l2',tol=0.0001)
+LR.fit(x,y)
+
+print(LR.predict(x))
