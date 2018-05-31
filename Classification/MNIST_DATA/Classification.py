c7c495cf095f87b3536f8b29e1a9a41db928f477
SirawichDev <sirawit0676@gmail.com>
2018-05-31 20:13:51 +0700

    using np.where find where is 2 value in x

--- Classification/MNIST_DATA/Classification.py
+++ Classification/MNIST_DATA/Classification.py
@@ -1,15 +1,23 @@
 from sklearn.datasets import fetch_mldata
+import numpy as np
 import matplotlib.pyplot as plt
 
 mnist = fetch_mldata('MNIST original')
 
-print(mnist)
-print(len(mnist['data'])) 
+# print(mnist)
+# print(len(mnist['data'])) 
 
 x , y = mnist['data'], mnist['target']
+print(x.shape) #70000 row กับ 784 pixel
 
 # print(x[69999])
 # print("result is : ", y[69999])
+print(np.where( y == 2 )) #show where nu,bbber 2 is
+show = x[12665] # 2
+print(y[12665]) #2
+image =   show.reshape(28,28) #28*28 = 784 #สูง * กว้าง
 
+plt.imshow(image)
+plt.show()
 
 
