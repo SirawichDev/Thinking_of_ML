f4570e8fe81d2b3aa880337cf1e690e644354a11
SirawichDev <sirawit0676@gmail.com>
2018-05-31 18:12:14 +0700

    using sklearn logistic regression

--- Classification/idea/logic_regression.py
+++ Classification/idea/logic_regression.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn
 
 #using sigmoid naja
-x = np.linspace(-10, 10, num=1000)
+x = np.linspace(-10, 10, num=50)
 plt.figure(figsize= (12,3))
 plt.plot( x, 1/ (1+np.exp(-x)))
 plt.title("Hello Sigmoid")
