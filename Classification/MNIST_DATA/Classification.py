84f391cc1172bd01ac53b766d3d17d9fb8bcf7d0
SirawichDev <sirawit0676@gmail.com>
2018-05-31 20:29:48 +0700

    start train and test

--- Classification/MNIST_DATA/Classification.py
+++ Classification/MNIST_DATA/Classification.py
@@ -12,12 +12,16 @@ print(x.shape) #70000 row กับ 784 pixel
 
 # print(x[69999])
 # print("result is : ", y[69999])
-print(np.where( y == 2 )) #show where nu,bbber 2 is
-show = x[12665] # 2
-print(y[12665]) #2
-image =   show.reshape(28,28) #28*28 = 784 #สูง * กว้าง
+# print(np.where( y == 2 )) #show where nu,bbber 2 is
+# show = x[12665] # 2
+# print(y[12665]) #2
+# image =   show.reshape(28,28) #28*28 = 784 #สูง * กว้าง
 
-plt.imshow(image)
-plt.show()
+# plt.imshow(image)
+# plt.show()
+num_split = 60000
 
+x_train, x_test, y_train, y_test = x[:num_split],x[num_split:],y[:num_split], y[num_split:]
+
+print(x_train)
 
