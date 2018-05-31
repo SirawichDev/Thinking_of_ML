8766d8f91b2e29eb0ceb61f23f0a9127144212eb
SirawichDev <sirawit0676@gmail.com>
2018-05-31 18:51:46 +0700

    use np stack for compare result and predictr

--- Classification/Sklearn/sk_regression.py
+++ Classification/Sklearn/sk_regression.py
@@ -9,9 +9,10 @@ dataset = [[-3.215, 0],
                 [4.5122,1]]
 
 x = np.array(dataset)[:, 0:1]
-y = np.array(dataset)[:,1]
-print(x)
+y = np.array(dataset)[:,1] #ค่าจริง
+#print(x)
 LR = LogisticRegression(C=1.0 , penalty='l2',tol=0.0001)
 LR.fit(x,y)
-
-print(LR.predict(x))
+y_predict = LR.predict(x)
+print("right side is Predict_y \n",np.column_stack((y,y_predict)))
+#print(LR.predict(x))
